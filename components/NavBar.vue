6a8dd4b73cfb10fbe2a69cc5caf5a722de64dd5c
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-17 22:02:42 +0100

    Changed Layoutes file

---
+++ components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div>
+        <div>
+            <img src="/images/logo-devlinks-small.svg" />
+            <h1>devlinks</h1>
+
+        </div>
+
+        <NuxtLink to="/editor" >Links</NuxtLink>
+        <NuxtLink to="/profile">Profile Details</NuxtLink>
+
+
+        <NuxtLink to="/preview">Preview</NuxtLink>
+    </div>
+
+
+
+
+
+</template>
+
+
+
+<script setup>
+
+   const props = defineProps({
+        isLink: Boolean
+    });
+
+
+</script>
+
+
+<style scoped>
+    div{
+        margin: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+</style>
