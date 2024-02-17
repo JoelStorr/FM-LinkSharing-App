5d81d44dbb6e708d5e562eb71f749e193ba33715
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-18 00:09:40 +0100

    Added html for profile editor

--- components/Profile/editor.vue
+++ components/Profile/editor.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Profile editor</h1>
+    <h2>Profile editor</h2>
+    <p>Add your details to create a personal touch to your profile.</p>
+    <div>
+        <p>Profile Picture</p>
+        <div>
+            <img src="/images/icon-upload-image.svg" />
+            <p>+ Upload Image</p>
+        </div>
+
+        <p>Images must be below 1024x1024px. <br/> Use PNG or JPG format.</p>
+    </div>
+    <div>
+        <form action="">
+            <label for="firstName">
+                First name*
+                <input id="firstName" placeholder="e.g John">
+            </label>
+            <label for="lastName">
+                Last name*
+                <input id="lastName" placeholder="e.g. Appleseed">
+            </label>
+            <label for="email">
+                Email
+                <input id="email" placeholder="e.g. email@example.com" />
+            </label>
+        </form>
+    </div>
+    <hr/>
+    <button>Save</button>
+
 </template>
