1b820f9eee966449d00932df58105eeb257571be
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-18 20:38:50 +0100

    Added Tab component

---
+++ components/UI/Tab.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <button class="tag" :class="props.active ? 'active' : ''">
+        <img :src="props.imgSrc" />
+        <p>{{ props.text }}</p>
+    </button>
+
+</template>
+
+<script setup>
+    const props = defineProps({
+        imgSrc: String,
+        text: String,
+        active: Boolean
+    });
+
+
+</script>
+
+<style scoped>
+    .tag{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: .25rem 1rem;
+        background-color: rgba(0,0,0,0);
+        border: none;
+        border-radius: .5rem;
+    }
+
+    .tag p{
+        margin: 0;
+        padding: 0;
+        margin-left: .5rem;
+        color: #737373;
+    }
+
+    .tag:hover p {
+        color: #633CFF;
+    }
+
+    .tag:hover img{
+        filter: brightness(0) saturate(100%) invert(27%) sepia(93%) saturate(3954%) hue-rotate(245deg) brightness(97%) contrast(108%);
+    }
+
+    .active{
+        background-color: #EFEBFF;
+    }
+
+    .active p{
+        color: #633CFF;
+    }
+
+    .active img{
+        filter: brightness(0) saturate(100%) invert(27%) sepia(93%) saturate(3954%) hue-rotate(245deg) brightness(97%) contrast(108%);
+    }
+</style>
