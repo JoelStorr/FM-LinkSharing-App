18504078329968a1b644c7a05cb5ed7823c80fee
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-18 15:45:24 +0100

    Addedb custom button components

---
+++ components/UI/Button/Primary.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="emit('onClick')" :disabled="props.disabled">
+        <slot />
+    </button>
+
+</template>
+
+
+<script setup>
+
+
+     const props = defineProps({
+        disabled: Boolean
+    })
+
+    const emit = defineEmits(['onClick']);
+
+
+</script>
+
+<style scoped>
+
+    button{
+        background-color: #623CFF ;
+        border: none;
+        color: white;
+        padding: 1rem 5rem;
+        border-radius: 10px;
+    }
+
+    button:active {
+        background-color: #BEADFF;
+    }
+
+    button:disabled{
+        background-color: #EFEBFF;
+    }
+</style>
---
+++ components/UI/Button/Secondary.vue
@@ -0,0 +1,42 @@
+<template>
+    <button @click="emit('onClick')" :disabled="props.disabled">
+        <slot />
+    </button>
+
+</template>
+
+
+<script setup>
+
+    const props = defineProps({
+        disabled: Boolean
+    })
+
+    const emit = defineEmits(['onClick']);
+
+
+</script>
+
+<style scoped>
+
+    button{
+        background-color: rgba(0,0,0,0) ;
+        border: 1px solid #623CFF;
+        color: #623CFF;
+        padding: 1rem 5rem;
+        border-radius: 10px;
+    }
+
+    button:active {
+        background-color: #EFEBFF;
+        border: 1px solid #623CFF;
+        color: #623CFF;
+
+    }
+
+    button:disabled{
+        background-color: rgba(0,0,0,0);
+         border: 1px solid #EFEBFF;
+         color: #EFEBFF;
+    }
+</style>
