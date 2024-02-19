1adec77c41153f8152b10e509740b9650da0fa71
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-19 10:59:38 +0100

    Reworked nav structure

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,22 +1,19 @@
 <template>
 
-    <div>
-        <div>
+    <div id="navbar">
+        <div class="logo-holder">
             <img src="/images/logo-devlinks-small.svg" />
             <h1>devlinks</h1>
-
         </div>
 
-        <button @click="onLinkEditorClick" >Links</button>
-        <button  @click="onProfileEditorCLick">Profile Details</button>
+        <div class="tabs-holder">
+            <UITab img-src="/images/icon-links-header.svg" :active="true" @clicked="onLinkEditorClick">Links</UITab>
+            <UITab img-src="/images/icon-profile-details-header.svg" :active="false" @clicked="onProfileEditorCLick">Profile Details</UITab>
+        </div>
 
-
-        <NuxtLink to="/preview">Preview</NuxtLink>
+        <UIButtonSecondary @on-click="navigateToProfiel">Preview</UIButtonSecondary>
+        
     </div>
-
-
-
-
 
 </template>
 
@@ -36,18 +33,41 @@
     }
 
     function onProfileEditorCLick(){
+        console.log('Nav profile editor')
         emit("setProfileEditor")
+    }
+
+    function navigateToProfiel(){
+        navigateTo('/preview')
     }
 
 </script>
 
 
 <style scoped>
-    div{
-        margin: 20px;
+    #navbar{
+        margin: 0 2rem;
         display: flex;
         flex-direction: row;
         align-items: center;
+        justify-content: space-between;
+        background-color: white;
+        border-radius: 25px;
+        padding: 0 2rem;
+    }
+
+    .logo-holder{
+        display: flex;
+
+    }
+
+
+
+    img{margin: 0;  padding: 0;}
+
+    .tabs-holder {
+        min-width: 15vw;
+        display: flex;
         justify-content: space-between;
     }
 
