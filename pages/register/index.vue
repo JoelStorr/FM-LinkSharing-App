5e2013a9e79e42b9436386b188d60cb1a32986ac
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-19 10:19:10 +0100

    Added Registration form with design

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,45 +1,196 @@
 <template>
-    <img src="/images/logo-devlinks-large.svg"/>
-    <h1>devlinks</h1>
-    <div>
-        <h2>Create account</h2>
-        <p>Let's get you started sharing you links!</p>
-        <form @submit.prevent="login" >
-            <label for="email">
-                Email address
-                <input id="email" type="email" placeholder="eg.alex@email.com" v-model="email" />
-            </label>
-            <label for="password">
-                Create password
-                <input id="password" type="password" placeholder="At least 8 characters" v-model="password">
-            </label>
-            <label for="passwordConfirm">
-                Confirm password
-                <input id="passwordConfirm" type="password" placeholder="At least 8 characters" v-model="confirmPassword">
-            </label>
-            <p>Password must contain at least 8 characters</p>
-            <button type="submit">Create a new Accout</button>
-        </form>
+  <div id="login-screen">
+    <img src="/images/logo-devlinks-large.svg" />
 
-        <p>Already have an account? <NuxtLink to="/login">Login</NuxtLink> </p>
+    <div id="login-content-holder">
+      <h1>Create account</h1>
+      <p>Let's get you started sharing your links!</p>
+      <form @submit.prevent="login" id="login-form">
+        <label for="email" class="g-bodyS">
+          Email address <br />
+          <UITextField
+            :error="isEmailError"
+            type="mail"
+            placeholder-text="e.g. alex@email.com"
+            field-id="email"
+            @on-value-change="saveEmail"
+          >
+            <img src="/images/icon-email.svg" />
+            <template v-slot:error>
+                {{ emailErrorText }}
+            </template>
+          </UITextField>
+        </label>
+
+        <label for="password" class="g-bodyS">
+          Password <br />
+          <UITextField
+            :error="isPasswordError"
+            type="password"
+            placeholder-text="Enter your password"
+            field-id="password"
+            @on-value-change="savePassword"
+          >
+            <img src="/images/icon-password.svg" />
+            <template v-slot:error>
+                {{ passwordErrorText }}
+            </template>
+          </UITextField>
+        </label>
+        <label for="confirmPassword" class="g-bodyS">
+          Confirm Password <br />
+          <UITextField
+            :error="isConfirmPasswordError"
+            type="password"
+            placeholder-text="Confirm your password"
+            field-id="confirmPassword"
+            @on-value-change="saveConfirmPassword"
+          >
+            <img src="/images/icon-password.svg" />
+            <template v-slot:error>
+                {{ confirmPasswordErrorText }}
+            </template>
+          </UITextField>
+        </label>
+        <p>Password must contain at least 8 characters</p>
+        <UIButtonPrimary type="submit"> Create new Account </UIButtonPrimary>
+      </form>
+
+      <p id="form-switch">
+        Alredy have an account? 
         
+        <NuxtLink to="/login" class="login-link">Login</NuxtLink>
+      </p>
     </div>
-    
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 definePageMeta({
-    layout: "noHeader"
-})
+  layout: "noHeader"
+});
 
-const email = ref("")
-const password = ref("")
-const confirmPassword = ref("")
+const email = ref("");
+const password = ref("");
+const confirmPassword = ref("");
 
-function login(){
+const isEmailError = ref(false);
+const isPasswordError = ref(false)
+const isConfirmPasswordError = ref(false)
+
+const emailErrorText = ref("")
+const passwordErrorText = ref("")
+const confirmPasswordErrorText = ref("")
+
+function saveEmail(value){
+    console.log(value);
+    email.value = value;
+};
+
+function savePassword(value){
+    console.log(value);
+    password.value = value;
+};
+
+function saveConfirmPassword(value){
+    console.log(value);
+    confirmPassword.value = value;
+};
+
+
+function login() {
+    console.log('submitedt form');
+
+    isEmailError.value = false
+    isPasswordError.value = false
+    isConfirmPasswordError.value = false
+
+    if(email.value.length < 4){
+        emailErrorText.value = "Please enter a email adress";
+        isEmailError.value = true;
+    } else{
+        emailErrorText.value = ""
+
+    }
+    if(password.value.length < 8){
+        passwordErrorText.value ="Pleas enter a valid password"
+        isPasswordError.value = true;
+    }else{
+        passwordErrorText.value = ""
+    }
+
+    if(confirmPassword.value != password.value){
+        confirmPasswordErrorText.value = "Pleas enter a valid password"
+        isConfirmPasswordError.value = true;
+    }else if (confirmPassword.value.length < 8){
+        confirmPasswordErrorText.value = "Both passwords need to match"
+        isConfirmPasswordError.value = true;
+    }else {
+        confirmPasswordErrorText.value = ""
+    }
+
+    if(isEmailError.value || isPasswordError.value || isConfirmPasswordError.value){
+        return
+    }
+
+   navigateTo("/editor");
+    
+
+
+
 
 }
-
 </script>
+
+<style scoped>
+#login-screen {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fafafa;
+}
+
+#login-content-holder {
+  overflow: hidden;
+  background-color: white;
+  width: 30vw;
+  padding: 4rem;
+  margin-top: 6rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+#login-form {
+  position: relative;
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  
+}
+
+#login-form label {
+  margin-bottom: 2rem;
+}
+
+#form-switch {
+  align-self: center;
+}
+
+.login-link {
+  text-decoration: none;
+}
+
+h1 {
+  margin: 0;
+  padding: 0;
+}
+</style>
