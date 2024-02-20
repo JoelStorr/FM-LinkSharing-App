5c87a05c46193b6d39b4eee8c773f7da621fe070
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-20 16:54:47 +0100

    Added Helper file

---
+++ components/Helper.js
@@ -0,0 +1,25 @@
+
+
+export class LinkObject {
+
+
+    name;
+    iconSrc;
+    link;
+
+    constructor(name, iconSrc, link  ){
+        this.name = name;
+        this.iconSrc = iconSrc;
+        this.link = link;
+    }
+
+
+
+}
+
+
+export const LinkOptions = {
+  GitHub: "/images/icon-github.svg",
+  YouTube: "/images/icon-youtube.svg",
+  LinkedIn: "/images/icon-linkedin.svg",
+};
