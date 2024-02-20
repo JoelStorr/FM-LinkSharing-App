bafd6c5ca3207af8b2d14e758be9bea4eccca3bb
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-20 13:31:31 +0100

    Clean up

--- components/UI/Link.vue
+++ components/UI/Link.vue
@@ -1,75 +1,63 @@
 <template>
-<div class="link" :class="bgColor">
-    <img :src="props.iconSrc" class="icon"/>
+  <div class="link" :class="bgColor">
+    <img :src="props.iconSrc" class="icon" />
     <p>{{ props.name }}</p>
-    <img src="/images/icon-arrow-right.svg"/>
-</div>
-
-
+    <img src="/images/icon-arrow-right.svg" />
+  </div>
 </template>
 
-
 <script setup>
+const props = defineProps({
+  iconSrc: String,
+  name: String,
+});
 
-    const props = defineProps({
-        iconSrc: String,
-        name: String
-    });
+console.log(typeof props.name);
 
-    console.log(typeof(props.name))
-
-    const bgColor = computed(()=>{
-        switch(props.name){
-            case 'GitHub':
-                return "black";
-            case "YouTube":
-                return "red";
-            case "LinkedIn":
-                return "blue";
-            default:
-                return "blue"
-        }
-
-    })
-
-
-
+const bgColor = computed(() => {
+  switch (props.name) {
+    case "GitHub":
+      return "black";
+    case "YouTube":
+      return "red";
+    case "LinkedIn":
+      return "blue";
+  }
+});
 </script>
 
-
 <style scoped>
-    .link{
-        background-color: black;
-        display: flex;
-        width: 100%;
-        border-radius: .5rem;
-        padding: 0 1rem;
-    }
+.link {
+  background-color: black;
+  display: flex;
+  width: 100%;
+  border-radius: 0.5rem;
+  padding: 0 1rem;
+}
 
-    .link:hover{
-        cursor: pointer;
-    }
+.link:hover {
+  cursor: pointer;
+}
 
-    p{
-        margin: 1rem 1rem;
-        flex: 1;
-        color: white;
-    }
+p {
+  margin: 1rem 1rem;
+  flex: 1;
+  color: white;
+}
 
-    .icon{
-        filter: brightness(0) saturate(100%) invert(100%) sepia(1%) saturate(44%) hue-rotate(56deg) brightness(114%) contrast(100%);
-        scale: 1.2;
-    }
+.icon {
+  filter: brightness(0) saturate(100%) invert(100%) sepia(1%) saturate(44%)
+    hue-rotate(56deg) brightness(114%) contrast(100%);
+  scale: 1.2;
+}
 
-    .black{
-        background-color: #1A1A1A;
-    }
-    .red{
-        background-color: #EE3939;
-    }
-    .blue{
-        background-color: #2D68FF;
-    }
-
-
+.black {
+  background-color: #1a1a1a;
+}
+.red {
+  background-color: #ee3939;
+}
+.blue {
+  background-color: #2d68ff;
+}
 </style>
--- pages/editor/index.vue
+++ pages/editor/index.vue
@@ -9,17 +9,48 @@
     <div class="mainView">
       <div class="phonePreview">
         <div class="image-holder">
-          <img src="/images/illustration-phone-mockup.svg"  class="phone-image"/>
+          <img
+            src="/images/illustration-phone-mockup.svg"
+            class="phone-image"
+          />
           <idv class="profile-image-holder">
-            <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="profile-image"/>
+            <img
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              class="profile-image"
+            />
           </idv>
-           <h2 class="name-prev">Max Mustermann</h2>
+          <h2 class="name-prev">Max Mustermann</h2>
           <p class="link-prev">max@example.com</p>
-          <UILink v-if="link1 != null" id="link1" :iconSrc="link1.iconSrc" :name="link1.name" />
-          <UILink v-if="link1 != null" id="link2" iconSrc="/images/icon-github.svg" name="YouTube" />
-          <UILink v-if="link1 != null" id="link3" iconSrc="/images/icon-github.svg" name="GitHub" />
-          <UILink v-if="link1 != null" id="link4" iconSrc="/images/icon-github.svg" name="YouTube" />
-          <UILink v-if="link1 != null" id="link5" iconSrc="/images/icon-github.svg" name="LinkedIn" />
+          <UILink
+            v-if="link1 != null"
+            id="link1"
+            :iconSrc="link1.iconSrc"
+            :name="link1.name"
+          />
+          <UILink
+            v-if="link1 != null"
+            id="link2"
+            iconSrc="/images/icon-github.svg"
+            name="YouTube"
+          />
+          <UILink
+            v-if="link1 != null"
+            id="link3"
+            iconSrc="/images/icon-github.svg"
+            name="GitHub"
+          />
+          <UILink
+            v-if="link1 != null"
+            id="link4"
+            iconSrc="/images/icon-github.svg"
+            name="YouTube"
+          />
+          <UILink
+            v-if="link1 != null"
+            id="link5"
+            iconSrc="/images/icon-github.svg"
+            name="LinkedIn"
+          />
         </div>
       </div>
       <div class="editor">
@@ -35,12 +66,11 @@ import { ref } from "vue";
 
 const isLinkEditor = ref(true);
 
-const link1 = ref({iconSrc:'/images/icon-github.svg', name: 'GitHub'})
-const link2 = ref(null)
-const link3 = ref(null)
-const link4 = ref(null)
-const link5 = ref(null)
-
+const link1 = ref({ iconSrc: "/images/icon-github.svg", name: "GitHub" });
+const link2 = ref(null);
+const link3 = ref(null);
+const link4 = ref(null);
+const link5 = ref(null);
 
 function setLinkEditor() {
   isLinkEditor.value = true;
@@ -78,18 +108,17 @@ function btnClick() {
   position: relative;
 }
 
-.image-holder{
+.image-holder {
   position: relative;
   width: min-content;
-  border: 3px solid green;
 }
 
-.phone-image{
+.phone-image {
   position: relative;
 }
 
-.profile-image-holder{
-   position: absolute;
+.profile-image-holder {
+  position: absolute;
   top: 17.5%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -98,16 +127,15 @@ function btnClick() {
   overflow: hidden;
   border-radius: 100px;
   opacity: 1;
-  border: 4px solid #623CFF;
+  border: 4px solid #623cff;
 }
-.profile-image{
+.profile-image {
   object-fit: cover;
   width: 100%;
   height: 100%;
-
 }
 
-.link-prev{
+.link-prev {
   position: absolute;
   top: 32%;
   left: 50%;
@@ -115,7 +143,7 @@ function btnClick() {
   background-color: white;
 }
 
-.name-prev{
+.name-prev {
   position: absolute;
   top: 28%;
   left: 50%;
@@ -125,44 +153,41 @@ function btnClick() {
   text-align: center;
 }
 
-#link1{
+#link1 {
   position: absolute;
   top: 44%;
   left: 11%;
   width: 78%;
-  opacity: .5;
 }
 
-#link2{
+#link2 {
   position: absolute;
   top: 54%;
   left: 11%;
   width: 78%;
   opacity: 1;
 }
-#link3{
+#link3 {
   position: absolute;
   top: 64%;
   left: 11%;
   width: 78%;
   opacity: 1;
 }
-#link4{
+#link4 {
   position: absolute;
   top: 74%;
   left: 11%;
   width: 78%;
   opacity: 1;
 }
-#link5{
+#link5 {
   position: absolute;
   top: 84%;
   left: 11%;
   width: 78%;
   opacity: 1;
 }
-
-
 
 .mainView {
   display: flex;
