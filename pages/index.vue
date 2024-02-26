0ccf8752ed9dba138314642bf5602c7d7172f701
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-26 21:15:09 +0100

    Added request for access token

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,55 @@
 <template>
   <h1>Index Page</h1>
+  {{ data }}
 </template>
 
 <script setup>
-definePageMeta({
-  middleware: [
-    function (to, from) {
-      return navigateTo("/editor");
-    },
-  ],
-});
+// definePageMeta({
+//   middleware: [
+//     function (to, from) {
+//       return navigateTo("/editor");
+//     },
+//   ],
+// });
+
+import { useMainStore } from "~/store/index";
+
+const store = useMainStore();
+const { add, remove, getShareProfile, loginUser } = store;
+
+// async function loginUser(email, password) {
+//   const myHeaders = new Headers();
+//   myHeaders.append("Accept", "application/json");
+//   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+//   const formdata = new FormData();
+//   formdata.append("username", email);
+//   formdata.append("password", password);
+
+//   const requestOptions = {
+//     method: "POST",
+//     headers: myHeaders,
+//     body: formdata,
+//     redirect: "follow",
+//   };
+
+//   await fetch("http://127.0.0.1:8000/auth/token", requestOptions)
+//     .then((response) => response.text())
+//     .then((result) => console.log(result))
+//     .catch((error) => console.error(error));
+// }
+
+
+  const formdata = new FormData();
+  formdata.append("username", "test@test.com");
+  formdata.append("password", "test1234567890");
+
+const axios = useNuxtApp().$axios
+
+let data = await loginUser("test@test.com","test1234567890")
+
+
+
 </script>
 
 <style>
--- store/index.js
+++ store/index.js
@@ -1,72 +1,87 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
+import axios from 'axios';
+
+const URL = "http://127.0.0.1:8000";
+
+export const useMainStore = defineStore("main", {
+  state: () => ({
+    links: [],
+    profile: {
+      firstName: null,
+      lastName: null,
+      email: null,
+      image: null,
+    },
+  }),
+
+  actions: {
+    // NOTE:Links
+    addEmpty() {
+      this.links.push({
+        id: Math.random() * 10000,
+        name: null,
+        icon: null,
+        link: null,
+        placeholder: null,
+      });
+    },
+
+    add(link) {
+      const index = this.links.map((e) => e.id).indexOf(link.id);
+      this.links[index].name = link.name;
+      this.links[index].icon = link.icon;
+      this.links[index].link = link.link;
+      this.links[index].placeholder = link.placeholder;
+    },
+
+    remove(id) {
+      const index = this.links.map((e) => e.id).indexOf(id);
+      this.links.splice(index, 1);
+    },
+
+    reoderLinks(reorder) {
+      this.links = reorder;
+    },
+
+    getIndex(id) {
+      return this.links.map((e) => e.id).indexOf(id);
+    },
+
+    // NOTE: Profile
+    addFristName(firstName) {
+      this.profile.firstName = firstName;
+    },
+    addLastName(lastName) {
+      this.profile.lastName = lastName;
+    },
+    addEmail(email) {
+      this.profile.email = email;
+    },
+
+    addImage(image) {
+      this.profile.image = image;
+    },
+
+    // NOTE: API Handling
+    async loginUser(email, password) {
+      const formdata = new FormData();
+      formdata.append("username", email);
+      formdata.append("password", password);
 
 
 
-export const useMainStore = defineStore('main', {
-    state: ()=>({
-        links: [],
-        profile: {
-            firstName: null,
-            lastName: null,
-            email: null,
-            image: null,
-        }
-    }),
+      let {data} = await axios.post("http://127.0.0.1:8000/auth/token", formdata);
+      return data
+    },
 
-    actions: {
-
-        // NOTE:Links
-        addEmpty(){
-            this.links.push({
-                id: Math.random() * 10000,
-                name: null,
-                icon: null,
-                link: null, 
-                placeholder: null,
-            })
-        },
-
-        add(link) {
-            const index = this.links.map((e) => e.id).indexOf(link.id);
-            this.links[index].name = link.name
-            this.links[index].icon = link.icon
-            this.links[index].link = link.link
-            this.links[index].placeholder = link.placeholder
-
-        },
-        
-        remove( id ) {
-            const index = this.links.map((e) => e.id).indexOf(id);
-            this.links.splice(index, 1);
-        },
-
-        reoderLinks(reorder){
-            this.links = reorder
-        },
-
-        getIndex(id){
-            return this.links.map((e) => e.id).indexOf(id);
-        },
-
-
-        // NOTE: Profile
-        addFristName(firstName){
-            this.profile.firstName = firstName;
-        },
-        addLastName(lastName){
-            this.profile.lastName = lastName;
-        },
-        addEmail(email){
-            this.profile.email = email
-        },
-
-        addImage(image){
-            this.profile.image = image
-        },
-
-      
-
-    }
-})
-
-
+    async getShareProfile() {
+      try {
+        let { data } = await useFetch(`${URL}/share/${"Joel-Storr-1"}`);
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+});
